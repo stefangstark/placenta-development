1d37cefba7dcad5d1a22b4ec204e4b3818d9d21a
Stefan G Stark <starks@inf.ethz.ch>
2023-10-29 17:59:54 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+
+from setuptools import setup, find_packages
+import os
+
+setup(name='irtoolkit',
+      version='0.1',
+      description='Toolkit for chemical imaging',
+      url='https://github.com/stefangstark/mouse-placenta-development',
+      author='Stefan G. Stark',
+      author_email='starks@ethz.ch',
+      license='BSD',
+      packages=find_packages(),
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=[],
+      zip_safe=False,
+)
